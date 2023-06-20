1dfa848831011057fa621ad8dd50e5785f49ec57
Ragghawan <75121305+Ragghawan@users.noreply.github.com>
2023-06-20 19:47:10 +0530

    Update ItemController.java

--- src/main/java/com/lowes/leap/itemmanagement/controller/ItemController.java
+++ src/main/java/com/lowes/leap/itemmanagement/controller/ItemController.java
@@ -19,7 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "leap-frontend-production.up.railway.app
+")
 @RestController
 @RequestMapping("/api/item")
 public class ItemController {

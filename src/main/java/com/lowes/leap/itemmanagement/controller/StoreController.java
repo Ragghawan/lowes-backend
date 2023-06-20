83b4704556b331303a9453284a0ff8c0991c2fb9
Ragghawan <75121305+Ragghawan@users.noreply.github.com>
2023-06-20 20:05:35 +0530

    Update StoreController.java

--- src/main/java/com/lowes/leap/itemmanagement/controller/StoreController.java
+++ src/main/java/com/lowes/leap/itemmanagement/controller/StoreController.java
@@ -17,8 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "leap-frontend-production.up.railway.app
-")
+@CrossOrigin(origins = "leap-frontend-production.up.railway.app")
 @RestController
 @RequestMapping("/api/store")
 public class StoreController {
